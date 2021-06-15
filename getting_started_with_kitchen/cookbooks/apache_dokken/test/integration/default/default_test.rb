bd3753a9c8241a8312cffdc26287ff3947dd1717
Varun Sharma <varunge@gmail.com>
2021-06-15 23:49:12 +0530

    added detailed test case for dokken cookbook

--- getting_started_with_kitchen/cookbooks/apache_dokken/test/integration/default/default_test.rb
+++ getting_started_with_kitchen/cookbooks/apache_dokken/test/integration/default/default_test.rb
@@ -7,11 +7,20 @@ describe package('httpd')  do
   it { should be_installed}
 end
 
-describe service('httpd') do
-  it { should be_running }
+# describe service('httpd') do
+#   it { should be_running }
+# end
+
+control 'http-1.0' do
+  impact 1.0
+  title 'Checking https status'
+  desc 'Checking https status'
+  describe service('httpd') do
+    it { should be_running }
+  end
 end
 
-describe command('curl locahost') do
+describe command('curl localhost') do
   its('stdout') { should match /hello/}
 end
 
