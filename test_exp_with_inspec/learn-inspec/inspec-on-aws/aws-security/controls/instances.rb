8de23d5c053cde69502da3b5c9b8ab01c337180b
Varun Sharma <varunge@gmail.com>
2021-06-16 23:17:12 +0530

    added few more tests

--- test_exp_with_inspec/learn-inspec/inspec-on-aws/aws-security/controls/instances.rb
+++ test_exp_with_inspec/learn-inspec/inspec-on-aws/aws-security/controls/instances.rb
@@ -1,7 +1,18 @@
 describe aws_ec2_instance(name: 'webserver') do
   it { should be_running }
+  its('image_id') { should eq 'ami-0ee02acd56a52998e' }
+  its('instance_type') { should eq 't2.micro' }
+  its('public_ip_address') { should eq '3.215.178.141' }
 end
 
-describe aws_ec2_instance('i-02cecf67a24151ce4') do
+describe aws_ec2_instance('i-03188940649402c10') do
   it { should be_running }
+  its('image_id') { should eq 'ami-0ee02acd56a52998e' }
+  its('instance_type') { should eq 't2.micro' }
+  its('public_ip_address') { should_not be ''}
+end
+
+describe aws_vpc('vpc-04250b13bfe66c3b8') do
+  its('state') { should eq 'available' }
+  its('cidr_block') { should eq '10.0.0.0/16' }
 end
