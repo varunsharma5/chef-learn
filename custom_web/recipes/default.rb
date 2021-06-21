147f45fe325d72d2d262bde5a772bf89492f3e88
Varun Sharma <varunge@gmail.com>
2021-06-21 19:36:04 +0530

    added recipes for installing apache

--- custom_web/recipes/default.rb
+++ custom_web/recipes/default.rb
@@ -3,3 +3,33 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+os_family = ""
+
+apt_update 'update apt database'
+
+package 'apache2'
+
+file '/var/www/html/index.html' do
+  content '<h1>Welcome home !</h1>'
+end
+
+directory '/srv/apache/admins/html' do
+  recursive true
+  mode '0755'
+end
+
+file '/srv/apache/admins/html/index.html' do
+  content '<h1>Welcome admins !</h1>'
+end
+
+template '/etc/apache2/sites-enabled/admins.conf' do
+  source 'admins.conf.erb'
+  mode '0644'
+  variables(document_root: '/srv/apache/admins/html', port: 8080)
+  notifies :reload, 'service[apache2]'
+end
+
+service 'apache2' do
+  action [:enable, :start]
+end
